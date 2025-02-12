7785c11c945f73df42c47908042c579d2f152716
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-02-12 19:32:33 +0000

    init commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,305 @@
-// Find all our documentation at https://docs.near.org
-use near_sdk::{log, near};
+use near_sdk::json_types::U128;
+use near_sdk::serde_json::{self, json};
+use near_sdk::{
+    env, log, near, require, AccountId, Gas, NearToken, PanicOnDefault, Promise, PromiseOrValue,
+    PromiseResult,
+};
+use std::str::FromStr;
 
-// Define the contract structure
+const META_MASK_DECIMALS: u8 = 18;
+const GAS_FOR_FT_ON_TRANSFER: Gas = Gas::from_tgas(20);
+const GAS_FOR_FT_TRANSFER: Gas = Gas::from_tgas(10);
+const GAS_FOR_FT_TRANSFER_CALL: Gas = Gas::from_tgas(50);
+const GAS_FOR_FT_RESOLVE: Gas = Gas::from_tgas(10);
+
+#[derive(Debug, PanicOnDefault)]
 #[near(contract_state)]
-pub struct Contract {
-    greeting: String,
+pub struct AuroraProxyToken {
+    token_id: AccountId,
+    decimals: u8,
 }
 
-// Define the default, which automatically initializes the contract
-impl Default for Contract {
-    fn default() -> Self {
-        Self {
-            greeting: "Hello".to_string(),
+#[near]
+impl AuroraProxyToken {
+    #[init]
+    #[allow(clippy::use_self)]
+    pub const fn new(token_id: AccountId, decimals: u8) -> Self {
+        Self { token_id, decimals }
+    }
+
+    #[payable]
+    #[allow(unused_variables)]
+    pub fn ft_transfer_call(
+        &mut self,
+        receiver_id: AccountId,
+        amount: U128,
+        memo: Option<String>, // Engine doesn't use memo
+        msg: String,
+    ) -> PromiseOrValue<U128> {
+        require!(
+            env::current_account_id() == receiver_id,
+            format!("Only on {} can call `ft_transfer_call`", self.token_id)
+        );
+
+        let (receiver_id, msg) = parse_message(&msg)
+            .map(|(receiver_id, msg)| {
+                (
+                    receiver_id.unwrap_or_else(|| env::panic_str("Receiver is not specified")),
+                    msg,
+                )
+            })
+            .unwrap_or_else(|| env::panic_str(&format!("Wrong message format: {msg}")));
+
+        let amount = self
+            .modify_amount(amount, self.withdraw_action())
+            .unwrap_or_else(|e| env::panic_str(e.as_ref()));
+
+        let promise = if msg.is_empty() {
+            log!(
+                "ft_transfer {} on token: {}, to {}",
+                amount.0,
+                &self.token_id,
+                &receiver_id,
+            );
+
+            Promise::new(self.token_id.clone()).function_call(
+                "ft_transfer".to_string(),
+                serde_json::to_vec(&json!({
+                    "receiver_id": receiver_id, // intents.near
+                    "amount": amount
+                }))
+                .unwrap(),
+                NearToken::from_yoctonear(1),
+                GAS_FOR_FT_TRANSFER,
+            )
+        } else {
+            log!(
+                "ft_transfer_call {} on token: {}, to {} with message {:?}",
+                amount.0,
+                &self.token_id,
+                receiver_id,
+                &msg
+            );
+
+            let Message { msg, memo } = serde_json::from_str(msg)
+                .unwrap_or_else(|_| env::panic_str(&format!("Wrong message format: {msg}")));
+
+            Promise::new(self.token_id.clone()).function_call(
+                "ft_transfer_call".to_string(),
+                serde_json::to_vec(&json!({
+                    "receiver_id": receiver_id, // intents.near
+                    "amount": amount,
+                    "memo": memo,
+                    "msg": msg,
+                }))
+                .unwrap(),
+                NearToken::from_yoctonear(1),
+                GAS_FOR_FT_TRANSFER_CALL,
+            )
+        };
+
+        PromiseOrValue::Promise(
+            promise.then(
+                Promise::new(env::current_account_id()).function_call(
+                    "ft_resolve_withdraw".to_string(),
+                    serde_json::to_vec(&json!({
+                        "sender_id": env::current_account_id(),
+                        "receiver_id": receiver_id,
+                        "amount": amount,
+                        "is_call": !msg.is_empty(),
+                    }))
+                    .unwrap(),
+                    NearToken::from_yoctonear(0),
+                    GAS_FOR_FT_RESOLVE,
+                ),
+            ),
+        )
+    }
+
+    pub fn ft_on_transfer(
+        &mut self,
+        sender_id: AccountId,
+        amount: U128,
+        msg: String,
+    ) -> PromiseOrValue<U128> {
+        // silo.aurora.near:fe71e451caabd1d87bdb75891430eb90a4956839
+        require!(
+            env::predecessor_account_id() == self.token_id,
+            format!("Only {} can call this method", self.token_id)
+        );
+
+        log!(
+            "ft_on_transfer {} from {} with message {}",
+            amount.0,
+            sender_id,
+            msg
+        );
+
+        let (engine_id, evm_receiver) = parse_message(&msg)
+            .unwrap_or_else(|| env::panic_str(&format!("Wrong message format: {msg}")));
+
+        let amount = self
+            .modify_amount(amount, self.deposit_action())
+            .unwrap_or_else(|e| env::panic_str(e.as_ref()));
+
+        PromiseOrValue::Promise(
+            Promise::new(engine_id.clone().unwrap())
+                .function_call(
+                    "ft_on_transfer".to_string(),
+                    serde_json::to_vec(&json!({
+                        "sender_id": env::current_account_id(),
+                        "amount": amount,
+                        "msg": evm_receiver,
+                    }))
+                    .unwrap(),
+                    NearToken::from_yoctonear(1),
+                    GAS_FOR_FT_ON_TRANSFER,
+                )
+                .then(
+                    Promise::new(env::current_account_id()).function_call(
+                        "ft_resolve_deposit".to_string(),
+                        serde_json::to_vec(&json!({
+                            "sender_id": env::current_account_id(),
+                            "receiver_id": engine_id,
+                            "amount": amount,
+                        }))
+                        .unwrap(),
+                        NearToken::from_yoctonear(0),
+                        GAS_FOR_FT_RESOLVE,
+                    ),
+                ),
+        )
+    }
+
+    #[private]
+    pub fn ft_resolve_withdraw(
+        &mut self,
+        sender_id: AccountId,
+        receiver_id: AccountId,
+        amount: U128,
+        is_call: bool,
+    ) -> U128 {
+        log!(
+            "ft_resolve_withdraw {} from {} to: {}",
+            amount.0,
+            sender_id,
+            receiver_id,
+        );
+
+        let used = match env::promise_result(0) {
+            PromiseResult::Successful(value) => {
+                if is_call {
+                    // `ft_transfer_call` returns successfully transferred amount
+                    serde_json::from_slice::<U128>(&value)
+                        .unwrap_or_default()
+                        .0
+                        .min(amount.0)
+                } else if value.is_empty() {
+                    // `ft_transfer` returns empty result on success
+                    amount.0
+                } else {
+                    0
+                }
+            }
+            PromiseResult::Failed => {
+                if is_call {
+                    // do not refund on failed `ft_transfer_call` due to
+                    // NEP-141 vulnerability: `ft_resolve_transfer` fails to
+                    // read result of `ft_on_transfer` due to insufficient gas
+                    amount.0
+                } else {
+                    0
+                }
+            }
+        };
+
+        U128(used)
+    }
+
+    #[private]
+    pub fn ft_resolve_deposit(
+        &mut self,
+        sender_id: AccountId,
+        receiver_id: AccountId,
+        amount: U128,
+    ) -> U128 {
+        log!(
+            "ft_resolve_deposit {} from {} to: {}",
+            amount.0,
+            sender_id,
+            receiver_id,
+        );
+
+        let used = match env::promise_result(0) {
+            PromiseResult::Successful(value) => serde_json::from_slice::<U128>(&value)
+                .unwrap_or_default()
+                .0
+                .min(amount.0),
+            PromiseResult::Failed => amount.0,
+        };
+
+        U128(used)
+    }
+
+    fn modify_amount(&self, amount: U128, action: Action) -> Result<U128, Error> {
+        match action {
+            Action::Decrease(decimals) => {
+                let amount = amount.0.saturating_div(10u128.pow(decimals as u32));
+                require!(amount > 0, Error::TooLowDeposit.as_ref());
+
+                Ok(U128(amount))
+            }
+            Action::Increase(decimals) => amount
+                .0
+                .checked_mul(10u128.pow(decimals as u32))
+                .ok_or(Error::TooHighDeposit)
+                .map(U128),
+        }
+    }
+
+    const fn deposit_action(&self) -> Action {
+        if self.decimals < META_MASK_DECIMALS {
+            Action::Increase(META_MASK_DECIMALS - self.decimals)
+        } else {
+            Action::Decrease(self.decimals - META_MASK_DECIMALS)
+        }
+    }
+
+    const fn withdraw_action(&self) -> Action {
+        if self.decimals > META_MASK_DECIMALS {
+            Action::Increase(self.decimals - META_MASK_DECIMALS)
+        } else {
+            Action::Decrease(META_MASK_DECIMALS - self.decimals)
         }
     }
 }
 
-// Implement the contract structure
-#[near]
-impl Contract {
-    // Public method - returns the greeting saved, defaulting to DEFAULT_GREETING
-    pub fn get_greeting(&self) -> String {
-        self.greeting.clone()
-    }
-
-    // Public method - accepts a greeting, such as "howdy", and records it
-    pub fn set_greeting(&mut self, greeting: String) {
-        log!("Saving greeting: {greeting}");
-        self.greeting = greeting;
-    }
+#[near(serializers = [json])]
+struct Message {
+    msg: String,
+    memo: Option<String>,
 }
 
-/*
- * The rest of this file holds the inline tests for the code above
- * Learn more about Rust tests: https://doc.rust-lang.org/book/ch11-01-writing-tests.html
- */
-#[cfg(test)]
-mod tests {
-    use super::*;
+enum Action {
+    Decrease(u8),
+    Increase(u8),
+}
 
-    #[test]
-    fn get_default_greeting() {
-        let contract = Contract::default();
-        // this test did not call set_greeting so should return the default "Hello" greeting
-        assert_eq!(contract.get_greeting(), "Hello");
-    }
+fn parse_message(msg: &str) -> Option<(Option<AccountId>, &str)> {
+    msg.split_once(':')
+        .map(|(acc, msg)| (AccountId::from_str(acc).ok(), msg))
+}
 
-    #[test]
-    fn set_then_get_greeting() {
-        let mut contract = Contract::default();
-        contract.set_greeting("howdy".to_string());
-        assert_eq!(contract.get_greeting(), "howdy");
+enum Error {
+    TooLowDeposit,
+    TooHighDeposit,
+}
+
+impl AsRef<str> for Error {
+    fn as_ref(&self) -> &str {
+        match self {
+            Self::TooLowDeposit => "Deposit is too low",
+            Self::TooHighDeposit => "Deposit is too high",
+        }
     }
 }
--- tests/test_basics.rs
+++ tests/test_basics.rs
@@ -1,30 +1,254 @@
+use aurora_engine_sdk::types::near_account_to_evm_address;
+use aurora_engine_types::parameters::engine::{CallArgs, FunctionCallArgsV2};
+use aurora_engine_types::types::Address;
+use aurora_engine_types::U256;
+use near_sdk::json_types::U128;
+use near_sdk::{AccountId, NearToken};
+use near_workspaces::network::Sandbox;
+use near_workspaces::{Account, Contract, Worker};
 use serde_json::json;
 
-#[tokio::test]
-async fn test_contract_is_operational() -> Result<(), Box<dyn std::error::Error>> {
-    let contract_wasm = near_workspaces::compile_project("./").await?;
+const EXIT_TO_NEAR_PRECOMPILE: &str = "e9217bc70b7ed1f598ddd3199e80b093fa71124f";
+const STORAGE_DEPOSIT: NearToken = NearToken::from_yoctonear(1_250_000_000_000_000_000_000);
+const INIT_TOTAL_SUPPLY: U128 = U128(1_000_000_000);
 
-    test_basics_on(&contract_wasm).await?;
+#[tokio::test]
+async fn test_deposit_withdraw() -> anyhow::Result<()> {
+    let sandbox = near_workspaces::sandbox().await?;
+    let user = sandbox.dev_create_account().await?;
+    let token = deploy_fungible_token(&sandbox).await?;
+    let engine = deploy_aurora(&sandbox).await?;
+    let proxy = deploy_proxy(&sandbox, token.id(), 6).await?;
+
+    set_base_token(&engine, proxy.id()).await?;
+    storage_deposit(&token, proxy.id()).await?;
+    storage_deposit(&token, user.id()).await?;
+
+    let deposit = U128(1000);
+    transfer(&token, user.id(), deposit).await?;
+
+    let user_balance = balance(&token, user.id()).await?;
+    assert_eq!(user_balance, deposit);
+
+    let evm_recipient = near_account_to_evm_address(user.id().as_bytes());
+    make_deposit(
+        &user,
+        token.id(),
+        proxy.id(),
+        engine.id(),
+        evm_recipient,
+        deposit,
+    )
+    .await?;
+
+    let token_balance = balance(&token, token.id()).await?;
+    assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY.0 - deposit.0));
+
+    let user_balance = balance(&token, user.id()).await?;
+    assert_eq!(user_balance, U128(0));
+
+    let proxy_balance = balance(&token, proxy.id()).await?;
+    assert_eq!(proxy_balance, deposit);
+
+    let evm_balance = evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
+
+    make_withdraw(&user, engine.id(), proxy.id(), user.id(), evm_balance).await?;
+
+    let token_balance = balance(&token, token.id()).await?;
+    assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY.0 - deposit.0));
+
+    let proxy_balance = balance(&token, proxy.id()).await?;
+    assert_eq!(proxy_balance, U128(0));
+
+    let user_balance = balance(&token, user.id()).await?;
+    assert_eq!(user_balance, deposit);
+
+    let evm_balance = crate::evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_balance, U128(0));
+
     Ok(())
 }
 
-async fn test_basics_on(contract_wasm: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-    let sandbox = near_workspaces::sandbox().await?;
-    let contract = sandbox.dev_deploy(contract_wasm).await?;
-
-    let user_account = sandbox.dev_create_account().await?;
-
-    let outcome = user_account
-        .call(contract.id(), "set_greeting")
-        .args_json(json!({"greeting": "Hello World!"}))
+async fn deploy_proxy(
+    sandbox: &Worker<Sandbox>,
+    token_id: &AccountId,
+    decimals: u8,
+) -> anyhow::Result<Contract> {
+    let contract_wasm = near_workspaces::compile_project("./").await?;
+    let contract = sandbox.dev_deploy(&contract_wasm).await?;
+    let result = contract
+        .call("new")
+        .args_json(json!({"token_id": token_id, "decimals": decimals}))
         .transact()
         .await?;
-    assert!(outcome.is_success());
+    assert!(result.is_success(), "{result:?}");
 
-    let user_message_outcome = contract.view("get_greeting").args_json(json!({})).await?;
-    assert_eq!(user_message_outcome.json::<String>()?, "Hello World!");
+    Ok(contract)
+}
+
+async fn deploy_fungible_token(sandbox: &Worker<Sandbox>) -> anyhow::Result<Contract> {
+    let contract = sandbox
+        .dev_deploy(include_bytes!("../res/fungible-token.wasm"))
+        .await?;
+
+    let result = contract
+        .call("new")
+        .args_json(json!({
+            "owner_id": contract.id(),
+            "total_supply": INIT_TOTAL_SUPPLY,
+            "metadata": {
+                "spec": "ft-1.0.0",
+                "name": "Token",
+                "symbol": "TKN",
+                "decimals": 6
+            }
+        }))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success(), "{result:?}");
+
+    Ok(contract)
+}
+
+async fn deploy_aurora(sandbox: &Worker<Sandbox>) -> anyhow::Result<Contract> {
+    let contract = sandbox
+        .dev_deploy(include_bytes!("../res/aurora-mainnet-silo-3.8.0.wasm"))
+        .await?;
+    let result = contract
+        .call("new")
+        .args_json(json!({
+           "chain_id": 1_313_161_559,
+            "owner_id": contract.id(),
+            "upgrade_delay_blocks": 0,
+            "key_manager": contract.id(),
+            "initial_hashchain": null
+        }))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success());
+
+    Ok(contract)
+}
+
+async fn set_base_token(aurora: &Contract, token_id: &AccountId) -> anyhow::Result<()> {
+    #[near_sdk::near(serializers = [borsh])]
+    enum WithdrawSerialize {
+        Borsh,
+        Json,
+    }
+
+    let result = aurora
+        .call("set_eth_connector_contract_account")
+        .args_borsh((token_id, WithdrawSerialize::Borsh))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success(), "{result:?}");
 
     Ok(())
 }
 
+async fn storage_deposit(token: &Contract, account_id: &AccountId) -> anyhow::Result<()> {
+    let result = token
+        .call("storage_deposit")
+        .args_json(json!({"account_id": account_id }))
+        .deposit(STORAGE_DEPOSIT)
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success());
+    Ok(())
+}
 
+async fn balance(token: &Contract, account_id: &AccountId) -> anyhow::Result<U128> {
+    token
+        .call("ft_balance_of")
+        .args_json(json!({"account_id": account_id}))
+        .view()
+        .await?
+        .json::<U128>()
+        .map_err(Into::into)
+}
+
+async fn evm_balance(engine: &Contract, address: Address) -> anyhow::Result<U128> {
+    let result = engine
+        .call("get_balance")
+        .args(address.as_bytes().to_vec())
+        .view()
+        .await?;
+
+    Ok(U256::from_big_endian(&result.result).as_u128().into())
+}
+
+async fn make_deposit(
+    user: &Account,
+    token: &AccountId,
+    proxy: &AccountId,
+    engine: &AccountId,
+    evm_recipient: Address,
+    deposit: U128,
+) -> anyhow::Result<()> {
+    let result = user
+        .call(token, "ft_transfer_call")
+        .args_json(json!({
+            "receiver_id": proxy,
+            "amount": deposit,
+            "msg": format!("{}:{}", engine, evm_recipient.encode())
+        }))
+        .deposit(NearToken::from_yoctonear(1))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success(), "{result:?}");
+
+    Ok(())
+}
+
+async fn make_withdraw(
+    user: &Account,
+    engine_id: &AccountId,
+    proxy_id: &AccountId,
+    near_receiver_id: &AccountId,
+    amount: U128,
+) -> anyhow::Result<()> {
+    let value = U256::from(amount.0).to_big_endian();
+    let result = user
+        .call(engine_id, "call")
+        .args_borsh(CallArgs::V2(FunctionCallArgsV2 {
+            contract: Address::decode(EXIT_TO_NEAR_PRECOMPILE).unwrap(),
+            value,
+            input: [
+                &[0u8],
+                proxy_id.as_bytes(),
+                b":",
+                near_receiver_id.as_bytes(),
+                b":",
+            ]
+            .concat(),
+        }))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success(), "{result:?}");
+
+    Ok(())
+}
+
+async fn transfer(token: &Contract, account_id: &AccountId, amount: U128) -> anyhow::Result<()> {
+    let result = token
+        .call("ft_transfer")
+        .args_json(json!({
+            "receiver_id": account_id,
+            "amount": amount
+        }))
+        .deposit(NearToken::from_yoctonear(1))
+        .max_gas()
+        .transact()
+        .await?;
+    assert!(result.is_success(), "{result:?}");
+
+    Ok(())
+}
