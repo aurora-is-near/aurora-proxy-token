56943472d393e77029dd432f2a364db1d97d1883
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-02-13 14:14:24 +0000

    chore: test to check withdraw via ft_transfer_call

--- src/tests/workspaces.rs
+++ src/tests/workspaces.rs
@@ -45,10 +45,10 @@ async fn test_deposit_withdraw() -> anyhow::Result<()> {
     let proxy_balance = balance(&token, proxy.id()).await?;
     assert_eq!(proxy_balance, deposit);
 
-    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
-    assert_eq!(evm_user_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
+    let evm_bal = evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_bal, U128(deposit.0 * 10u128.pow(18 - 6)));
 
-    make_withdraw(&user, engine.id(), proxy.id(), user.id(), evm_user_balance).await?;
+    make_withdraw(&user, engine.id(), proxy.id(), user.id(), evm_bal, None).await?;
 
     let token_balance = balance(&token, token.id()).await?;
     assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY - deposit.0));
@@ -57,6 +57,78 @@ async fn test_deposit_withdraw() -> anyhow::Result<()> {
     assert_eq!(proxy_balance, U128(0));
 
     let user_balance = balance(&token, user.id()).await?;
+    assert_eq!(user_balance, deposit);
+
+    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_user_balance, U128(0));
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_deposit_withdraw_service() -> anyhow::Result<()> {
+    let sandbox = near_workspaces::sandbox().await?;
+    let Env {
+        token,
+        engine,
+        proxy,
+        ..
+    } = env(&sandbox, INIT_TOTAL_SUPPLY, 6).await?;
+    let service = sandbox
+        .dev_deploy_tla(include_bytes!("../../res/mock-service.wasm"))
+        .await
+        .unwrap();
+
+    set_base_token(&engine, proxy.id()).await?;
+    storage_deposit(&token, proxy.id()).await?;
+    storage_deposit(&token, service.id()).await?;
+
+    let deposit = U128(1000);
+    transfer(&token, service.id(), deposit).await?;
+
+    let user_balance = balance(&token, service.id()).await?;
+    assert_eq!(user_balance, deposit);
+
+    let evm_recipient = near_account_to_evm_address(service.id().as_bytes());
+    make_deposit(
+        service.as_account(),
+        token.id(),
+        proxy.id(),
+        engine.id(),
+        evm_recipient,
+        deposit,
+    )
+    .await?;
+
+    let token_balance = balance(&token, token.id()).await?;
+    assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY - deposit.0));
+
+    let user_balance = balance(&token, service.id()).await?;
+    assert_eq!(user_balance, U128(0));
+
+    let proxy_balance = balance(&token, proxy.id()).await?;
+    assert_eq!(proxy_balance, deposit);
+
+    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_user_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
+
+    make_withdraw(
+        service.as_account(),
+        engine.id(),
+        proxy.id(),
+        service.id(),
+        evm_user_balance,
+        Some(r#"{\"msg\":\"withdraw\",\"memo\":\"withdraw\"}"#.to_string()),
+    )
+    .await?;
+
+    let token_balance = balance(&token, token.id()).await?;
+    assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY - deposit.0));
+
+    let proxy_balance = balance(&token, proxy.id()).await?;
+    assert_eq!(proxy_balance, U128(0));
+
+    let user_balance = balance(&token, service.id()).await?;
     assert_eq!(user_balance, deposit);
 
     let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
@@ -85,13 +157,13 @@ async fn test_deposit_withdraw_two_acc() -> anyhow::Result<()> {
     let user_balance = balance(&token, user.id()).await?;
     assert_eq!(user_balance, deposit);
 
-    let evm_recipient = near_account_to_evm_address(user.id().as_bytes());
+    let recipient = near_account_to_evm_address(user.id().as_bytes());
     make_deposit(
         &user,
         token.id(),
         proxy.id(),
         engine.id(),
-        evm_recipient,
+        recipient,
         deposit,
     )
     .await?;
@@ -105,15 +177,15 @@ async fn test_deposit_withdraw_two_acc() -> anyhow::Result<()> {
     let proxy_balance = balance(&token, proxy.id()).await?;
     assert_eq!(proxy_balance, deposit);
 
-    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
+    let evm_user_balance = evm_balance(&engine, recipient).await?;
     assert_eq!(evm_user_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
 
     let user2 = "user2.near".parse()?;
     storage_deposit(&token, &user2).await?;
 
-    let withdraw_amount = U128(evm_user_balance.0 / 2);
-    make_withdraw(&user, engine.id(), proxy.id(), user.id(), withdraw_amount).await?;
-    make_withdraw(&user, engine.id(), proxy.id(), &user2, withdraw_amount).await?;
+    let amount = U128(evm_user_balance.0 / 2);
+    make_withdraw(&user, engine.id(), proxy.id(), user.id(), amount, None).await?;
+    make_withdraw(&user, engine.id(), proxy.id(), &user2, amount, None).await?;
 
     let token_balance = balance(&token, token.id()).await?;
     assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY - deposit.0));
@@ -127,7 +199,7 @@ async fn test_deposit_withdraw_two_acc() -> anyhow::Result<()> {
     let user2_balance = balance(&token, &user2).await?;
     assert_eq!(user2_balance, U128(deposit.0 / 2));
 
-    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
+    let evm_user_balance = evm_balance(&engine, recipient).await?;
     assert_eq!(evm_user_balance, U128(0));
 
     Ok(())
@@ -173,12 +245,12 @@ async fn test_withdraw_to_account_without_storage_deposit() -> anyhow::Result<()
     let proxy_balance = balance(&token, proxy.id()).await?;
     assert_eq!(proxy_balance, deposit);
 
-    let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
-    assert_eq!(evm_user_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
+    let evm_bal = evm_balance(&engine, evm_recipient).await?;
+    assert_eq!(evm_bal, U128(deposit.0 * 10u128.pow(18 - 6)));
 
     // Withdraw to account without storage deposit
     let user2 = "user2.near".parse()?;
-    make_withdraw(&user, engine.id(), proxy.id(), &user2, evm_user_balance).await?;
+    make_withdraw(&user, engine.id(), proxy.id(), &user2, evm_bal, None).await?;
 
     let token_balance = balance(&token, token.id()).await?;
     assert_eq!(token_balance, U128(INIT_TOTAL_SUPPLY - deposit.0));
@@ -292,8 +364,8 @@ async fn test_attempt_to_withdraw_more_tokens() -> anyhow::Result<()> {
     let evm_user_balance = evm_balance(&engine, evm_recipient).await?;
     assert_eq!(evm_user_balance, U128(deposit.0 * 10u128.pow(18 - 6)));
 
-    let withdraw_amount = U128(evm_user_balance.0 + 1);
-    make_withdraw(&user, engine.id(), proxy.id(), user.id(), withdraw_amount).await?;
+    let amount = U128(evm_user_balance.0 + 1);
+    make_withdraw(&user, engine.id(), proxy.id(), user.id(), amount, None).await?;
 
     // Nothing changed
     let token_balance = balance(&token, token.id()).await?;
